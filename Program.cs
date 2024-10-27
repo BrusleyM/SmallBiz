4f96fda1bd792205245c076f020adf8a675b84ec
vishnu <phashekeabetswe@gmail.com>
2024-10-27 12:47:06 +0200

    remove bin

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using SmallBizAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using SmallBizAPI.Repositories.Services;
 using SmallBizAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,15 @@ builder.Services.AddDbContext<SmallBizDBContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
 builder.Services.AddScoped<IUserRepository, UserService>();
 
 builder.Services.AddEndpointsApiExplorer();
